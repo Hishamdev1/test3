a4c5ec797b4019393f643f969836eaf00163990b
Hishamdev1 <hishamdev1@gmail.com>
2025-05-10 20:37:40 +0300

    Update main.dart.js

--- build/web/main.dart.js
+++ build/web/main.dart.js
@@ -1,1 +1,10 @@
-
+// Minimal "Flutter-like" render
+document.getElementById("app").innerHTML = `
+  <div style="
+    color: lime;
+    font-size: 80px;
+    font-weight: bold;
+    text-shadow: 0 0 20px green;
+    font-family: Arial, sans-serif;
+  ">HISHAM</div>
+`;
